d8e53cc62847ae5222ba30418987ac0222512015
Ukrit Fongsomboon <Ukrit.fb@gmail.com>
2023-12-22 02:04:06 +0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,21 @@ import Antd from 'ant-design-vue'
 import router from '@/routers'
 const pinia = createPinia()
 
-import UserRepo from '@/repositories/userAPI'
-import UserSrv from '@/services/userSrv'
 import Storage from '@/utils/storage/storageLS'
 import Request from '@/utils/request/requestAxios'
 
+import UserRepo from '@/repositories/userAPI'
+import UserSrv from '@/services/userSrv'
+
 const storage = new Storage()
 const request = new Request()
+
 const userRepo = new UserRepo()
 
 const userSrv = new UserSrv(userRepo, request, storage)
 
 const app = createApp(App)
-
 app.provide('$userSrv', userSrv)
-
 app.use(pinia)
 app.use(Antd)
 app.use(router)
